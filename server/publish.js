2ebec795ea6c6d720c0ce656fb4ede2c2ab12f9d
ErikRamses <erik.rmh@gmail.com>
2016-09-03 19:35:59 -0500

    LoginFacebook
    Login con facebook para desarrollo

--- server/publish.js
+++ server/publish.js
@@ -4,3 +4,9 @@ Meteor.publish('messages', function () {
 Meteor.publish("users", function () {
   return Users.find();
 });
+
+Meteor.publish("userData", function () {
+  return Meteor.users.find({_id: this.userId},
+    {fields: {'services': 1 }
+  });
+});
---
+++ server/social-config.js
@@ -0,0 +1,17 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+/* 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '786521788124677',
+    secret: 'c0c8b37d325e2b2768f944f38056edc7'
+});
+*/
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1746937965559748',
+    secret: 'f6fa6465d04217fcbe64916f20ad8438'
+});
