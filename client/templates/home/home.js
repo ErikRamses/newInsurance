877fa9b5def70ef6ebc08f49468890c819611532
nubestra <nubestra@gmail.com>
2016-09-19 11:05:40 -0500

    chat and login
    chat and login

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -2,13 +2,35 @@
 /* Home: Event Handlers */
 /*****************************************************************************/
 Template.Home.events({
+    'click img.chat': function (e) {
+        e.preventDefault()
+        var roomId = Meteor.user()._id
+        var username = Meteor.user().profile.name
+        if (!username) {
+            if ($('.form-group').hasClass('has-error'))
+                return
+            $('.form-group').addClass('has-error').append('<span class="help-block">Is required</span>')
+            $('input').keyup(function () {
+                if ($(this).val() != '') {
+                    $('.form-group').removeClass('has-error')
+                    $('span').remove()
+                }
+            })
+            return
+        }
+        console.log(username, roomId)
+        Router.go('room', {roomId: roomId}, {query: 'username=' + username})
+    }
 });
 
 /*****************************************************************************/
 /* Home: Helpers */
 /*****************************************************************************/
-Template.Home.helpers({
-});
+//Template.Home.helpers({
+//    'random': function () {
+//        return 
+//    }
+//});
 
 /*****************************************************************************/
 /* Home: Lifecycle Hooks */
