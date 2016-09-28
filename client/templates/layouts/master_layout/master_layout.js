a31d847331b88ca6bcef6176cf233fdead827a52
ErikRamses <erik.rmh@gmail.com>
2016-09-28 11:24:24 -0500

    InviteFriends
    Invitar amigos a descargar la app

--- client/templates/layouts/master_layout/master_layout.js
+++ client/templates/layouts/master_layout/master_layout.js
@@ -7,6 +7,19 @@ Template.MasterLayout.events({
         Meteor.logout();
         Router.go('login');
     }
+    //'click #invite': function() {
+  	//	var recipient = this.facebook_id;
+  	//	var config = Accounts.loginServiceConfiguration.findOne({service: 'facebook'});
+  		//var url = "http://www.facebook.com/dialog/feed?app_id=" + config.appId + "&display=popup&to=" + recipient + "&redirect_uri=" + Meteor.absoluteUrl('_fb?close');
+  		//window.open(url, "Create Post", "height=240,width=450,left=100,top=100");
+  		//FB.ui({
+    	//	method: 'share',
+    	//	display: 'popup',
+    	//	href: 'https://developers.facebook.com/docs/',
+  		//	}, function(response){
+
+  		//	});
+	//}
 });
 
 
