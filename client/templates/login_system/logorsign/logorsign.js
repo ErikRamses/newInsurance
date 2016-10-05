b71a2bc704b362c24a9331786c7505888065ce79
ErikRamses <erik.rmh@gmail.com>
2016-10-05 01:46:41 -0500

    TemplateResetPass
    Template resepas en inicio para reiniciar contraseña.

---
+++ client/templates/login_system/logorsign/logorsign.js
@@ -0,0 +1,33 @@
+Template.ResetPassword.events({
+  'submit #resetPasswordForm': function(e, t) {
+    e.preventDefault();
+    
+    var resetPasswordForm = $(e.currentTarget),
+        password = resetPasswordForm.find('#resetPasswordPassword').val(),
+        passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
+
+    if (isNotEmpty(password) && areValidPasswords(password, passwordConfirm)) {
+      Accounts.resetPassword(Session.get('resetPassword'), password, function(err) {
+        if (err) {
+          console.log('We are sorry but something went wrong.');
+        } else {
+          console.log('Your password has been changed. Welcome back!');
+          Session.set('resetPassword', null);
+        }
+      });
+    }
+    return false;
+  }
+});
+
+Template.logorsign.helpers({
+  resetPassword: function(){
+    if (Accounts._resetPasswordToken) {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+});
