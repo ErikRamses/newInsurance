9a41528f90509dcba127de68959e93567ba4d8e7
nubestra <nubestra@gmail.com>
2016-10-18 08:26:05 -0500

    Botón de choque
    Envía un mensaje y la app se pone en modo calma.

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -2,25 +2,6 @@
 /* Home: Event Handlers */
 /*****************************************************************************/
 Template.Home.events({
-    //'click img.chat': function (e) {
-    //    e.preventDefault()
-    //    var roomId = Meteor.user()._id
-    //    var username = Meteor.user().profile.name
-    //    if (!username) {
-    //        if ($('.form-group').hasClass('has-error'))
-    //            return
-    //        $('.form-group').addClass('has-error').append('<span class="help-block">Is required</span>')
-    //        $('input').keyup(function () {
-    //            if ($(this).val() != '') {
-    //                $('.form-group').removeClass('has-error')
-    //                $('span').remove()
-    //            }
-    //        })
-    //        return
-    //    }
-    //    console.log(username, roomId)
-    //    Router.go('room', {roomId: roomId}, {query: 'username=' + username})
-    //},
     'click img.llamada': function (e) {
       e.preventDefault()
       myApp.modal({
@@ -52,6 +33,25 @@ Template.Home.events({
           },
         ]
       });
+    },
+    'click img.choque': function (e) {
+      e.preventDefault()
+      $('#s-chat-submit-input').val($('#s-chat-submit-input').val() + 'Request for help on car accident');
+      $( ".s-chat-box" ).addClass( "opened" );
+      //emula enter
+      var e = jQuery.Event("keydown");
+      e.which = 13; // Enter
+      $("#s-chat-submit-input").trigger(e);
+      //emula enter
+
+      $(".typed").typed({
+        strings: ["Everything is gonna be alright.", "Take 3 deep breaths.^20000"],
+        typeSpeed: 50,
+        contentType: 'html',// or 'html'
+        loop: false,
+        showCursor: true,
+        backDelay: 2000,
+      });
     }
 });
 
@@ -73,16 +73,6 @@ Template.Home.onCreated(function () {
 });
 
 Template.Home.onRendered(function () {
-    //if (Session.get('isNewUser') == true) {
-    //    $(".typed").typed({
-    //      strings: ["Hello, welcome to Eichelman Insurance App.", "At bottom you can see 3 buttons.", "The red one will alert us about your situation, ^1000 let us help you.", "Bottom left button will call Eichelmann Insurance Agency.", "With bottom right button you can talk with a specialist.", "Top right button will show your profile, feel free to complete your data, this will allow us to help you more."],
-    //      typeSpeed: 50,
-    //      contentType: 'text',// or 'html'
-    //      loop: true,
-    //      showCursor: true,
-    //      backDelay: 2000,
-    //    });
-    //}
 });
 
 Template.Home.onDestroyed(function () {
