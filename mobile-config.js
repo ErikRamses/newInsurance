2688426502f25330f466b1339e5a28338f0884b7
nubestra <nubestra@gmail.com>
2016-10-18 21:33:30 -0500

    splash iPhone

--- mobile-config.js
+++ mobile-config.js
@@ -13,35 +13,31 @@ App.info({
 //  'android_xxxhdpi': 'resources/icons/mipmap-xxxhdpi/ic_launcher.png'
 //});
 
-//App.launchScreens({
+App.launchScreens({
   // iOS
-  //'iphone': 'resources/splash/splash-320x480.png',
-  //'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-  //'iphone5': 'resources/splash/splash-320x568@2x.png',
-  //'iphone6': 'resources/splash/splash-375x667@2x.png',
-  //'iphone6p_portrait': 'resources/splash/splash-414x736@3x.png',
-  //'iphone6p_landscape': 'resources/splash/splash-736x414@3x.png',
-  //'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  //'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  //'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  //'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
-
+  'iphone_2x': 'resources/splash/640x960@2x.png',
+  //'iphone5': 'resources/splash/640x1136@2x.png',
+  //'iphone6': 'resources/splash/750x1334@2x.png',
+  //'iphone6p_portrait': 'resources/splash/1242x2208@3x.png',
+  //'iphone6p_landscape': 'resources/splash/2208x1242@3x.png',
+  //'ipad_portrait': 'resources/splash/768x1024@3x.png',
+  //'ipad_portrait_2x': 'resources/splash/1536x2048@2x.png',
+  //'ipad_landscape': 'resources/splash/1024x768.png',
+  //'ipad_landscape_2x': 'resources/splash/2048x1536@2x.png',
   // Android
-  //'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  //'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  //'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png',
-  //'android_xxhdpi_portrait': 'resources/splash/splash-200x320.png',
-  //'android_xxhdpi_landscape': 'resources/splash/splash-320x200.png',
-  //'android_xxxdpi_portrait': 'resources/splash/splash-200x320.png',
-  //'android_xxxdpi_landscape': 'resources/splash/splash-320x200.png'
-//});
+  //'android_mdpi_portrait': 'resources/splash/drawable-mdpi/screen.png',
+  //'android_mdpi_landscape': 'resources/splash/drawable-land-mdpi/screen.png',
+  //'android_hdpi_portrait': 'resources/splash/drawable-hdpi/screen.png',
+  //'android_hdpi_landscape': 'resources/splash/drawable-land-hdpi/screen.png',
+  //'android_xhdpi_portrait': 'resources/splash/drawable-xhdpi/screen.png',
+  //'android_xhdpi_landscape': 'resources/splash/drawable-land-xhdpi/screen.png',
+  //'android_xxhdpi_portrait': 'resources/splash/drawable-xxhdpi/screen.png',
+  //'android_xxhdpi_landscape': 'resources/splash/drawable-land-xxhdpi/screen.png'
+});
 
-//App.setPreference('SplashScreen', 'CDVSplashScreen'); 
-//App.accessRule('*.google.com/*');
-//App.accessRule('*.googleapis.com/*');
-//App.accessRule('*.gstatic.com/*');
-//App.accessRule('*', true);
+App.setPreference('SplashScreen', 'CDVSplashScreen'); 
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
+App.accessRule('*', true);
 App.accessRule('https://*.stripe.com/*');
