2d083c9332997459d5e9e8252715cb4a3204171f
nubestra <nubestra@gmail.com>
2016-11-05 19:22:49 -0600

    Landing page get app
    Landing page para descargar la app en app para google play o en App
    Store, está disponible en /getapp

---
+++ client/templates/getapp/getapp.js
@@ -0,0 +1,21 @@
+Template.getapp.events({
+});
+
+/*****************************************************************************/
+/* getapp: Helpers */
+/*****************************************************************************/
+Template.getapp.helpers({
+});
+
+/*****************************************************************************/
+/* getapp: Lifecycle Hooks */
+/*****************************************************************************/
+Template.getapp.onCreated(function () {
+});
+
+Template.getapp.onRendered(function () {
+   $(".landing-background").backstretch("/img/pattern.png");
+});
+
+Template.getapp.onDestroyed(function () {
+});
