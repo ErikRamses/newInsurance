e49773cbc34af1c94ffe9db09d851b4197f6de35
nubestra <nubestra@gmail.com>
2016-11-15 21:41:09 -0600

    update
    así jala todo

--- client/templates/tpl-container/master_layout.js
+++ client/templates/tpl-container/master_layout.js
@@ -86,15 +86,4 @@ Template.MasterLayout.onRendered(function () {
     }); 
     $$ = Dom7;
   }
-  //si es ios carga algo, si es android y web otra cosa
-  if (is.ios()) {
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/dist/css/framework7.ios.css" >');
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/dist/css/framework7.ios.colors.css" >');
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/dist/css/framework7.ios.rtl.css" >');
-  } else {
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/framework7/dist/css/framework7.material.css" >');
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/framework7/dist/css/framework7.material.colors.css" >');
-      $('head').append('<link rel="stylesheet" type="text/css" href="/packages/nobutakaoshiro_framework7-ios-material/dist/css/framework7.material.rtl.css" >');
-  }
-
 });
