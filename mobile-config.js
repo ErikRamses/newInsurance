6fd81725e77b1b42c343c0c777f7373795cb6dc1
nubestra <nubestra@gmail.com>
2016-11-15 21:17:00 -0600

    mobile config actualizado
    mobile config actualizado :)

--- mobile-config.js
+++ mobile-config.js
@@ -15,30 +15,29 @@ App.icons({
   'android_xhdpi': 'resources/icons/android/res/drawable-xhdpi/icon.png',
   'android_xxhdpi': 'resources/icons/android/res/drawable-xxhdpi/icon.png',
   'android_xxxhdpi': 'resources/icons/android/res/drawable-xxxhdpi/icon.png',
-  'iphone': 'resources/icons/ios/res/Icon-App-60x60@1x.png',
-  'iphone_2x': 'resources/icons/ios/res/Icon-App-60x60@2x.png',
-  'iphone_3x': 'resources/icons/ios/res/Icon-App-76x76@2x.png',
-  'ipad': 'resources/icons/ios/res/Icon-App-76x76@2x.png',
-  'ipad_2x': 'resources/icons/ios/res/Icon-App-76x76@2x.png',
-  'ipad_pro': 'resources/icons/ios/res/Icon-App-76x76@2x.png',
-  'ios_settings': 'resources/icons/ios/res/Icon-App-29x29@2x.png',
-  'ios_settings_2x': 'resources/icons/ios/res/Icon-App-57x57@1x.png',
-  'ios_settings_3x': 'resources/icons/ios/res/Icon-App-83.5x83.5@2x.png',
-  'ios_spotlight': 'resources/icons/ios/res/Icon-App-40x40@2x.png',
-  'ios_spotlight_2x': 'resources/icons/ios/res/Icon-App-83.5x83.5@2x.png'
+  'iphone_2x': 'resources/icons/ios/res/iphone_2x.png',
+  'iphone_3x': 'resources/icons/ios/res/iphone_3x.png',
+  'ipad': 'resources/icons/ios/res/ipad.png',
+  'ipad_2x': 'resources/icons/ios/res/ipad_2x.png',
+  'ipad_pro': 'resources/icons/ios/res/ipad_pro.png',
+  'ios_settings': 'resources/icons/ios/res/ios_settings.png',
+  'ios_settings_2x': 'resources/icons/ios/res/ios_settings_2x.png',
+  'ios_settings_3x': 'resources/icons/ios/res/ios_settings_3x.png',
+  'ios_spotlight': 'resources/icons/ios/res/ios_spotlight.png',
+  'ios_spotlight_2x': 'resources/icons/ios/res/ios_spotlight_2x.png'
 });
 
 App.launchScreens({
   // iOS
-  'iphone_2x': 'resources/splash/640x960@2x.png',
-  'iphone5': 'resources/splash/640x1136@2x.png',
-  'iphone6': 'resources/splash/750x1334@2x.png',
-  'iphone6p_portrait': 'resources/splash/1242x2208@3x.png',
-  'iphone6p_landscape': 'resources/splash/2208x1242@3x.png',
-  'ipad_portrait': 'resources/splash/768x1024@3x.png',
-  'ipad_portrait_2x': 'resources/splash/1536x2048@2x.png',
-  'ipad_landscape': 'resources/splash/1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/2048x1536@2x.png',
+  'iphone_2x': 'resources/splash/iphone_2x.png',
+  'iphone5': 'resources/splash/iphone5.png',
+  'iphone6': 'resources/splash/iphone6.png',
+  'iphone6p_portrait': 'resources/splash/iphone6p_portrait.png',
+  'iphone6p_landscape': 'resources/splash/iphone6p_landscape.png',
+  'ipad_portrait': 'resources/splash/ipad_portrait.png',
+  'ipad_portrait_2x': 'resources/splash/ipad_portrait_2x.png',
+  'ipad_landscape': 'resources/splash/ipad_landscape.png',
+  'ipad_landscape_2x': 'resources/splash/ipad_landscape_2x.png',
   // Android
   'android_mdpi_portrait': 'resources/splash/drawable-mdpi/screen.png',
   'android_mdpi_landscape': 'resources/splash/drawable-land-mdpi/screen.png',
@@ -55,7 +54,6 @@ App.configurePlugin('cordova-plugin-facebook4', {
   APP_NAME: 'f6fa6465d04217fcbe64916f20ad8438'
 });
 
-App.setPreference('SplashScreen', 'CDVSplashScreen'); 
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
